d4f9f761fe197ae7ee2e86130cd010dd684be92d
Marcin Kruk <marcin.kruk@pricefx.com>
2020-04-19 14:43:40 +0200

    APBD3

---
+++ APBD3/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Wyklad3.Models;
+using Wyklad3.Services;
+
+namespace Wyklad3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            Console.WriteLine("post");
+            student.indexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+    }
+}
